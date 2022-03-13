d40591e072a2cddf4b5a3416d5db9a1dc8d9f862
anooplab <anoop@chem.iitkgp.ernet.in>
2022-03-13 14:23:52 +0530

    __init__.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='pyar-de',
     version='0.1',
-    packages=['pyar-de'],
+    packages=['src'],
     scripts=['pyar-de'],
     url='https://github.com/anooplab/pyar-de',
     license='GPL',
