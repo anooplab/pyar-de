888e05c99a95caa80e327aa470a7c17f0b118573
anooplab <anoop@chem.iitkgp.ernet.in>
2022-03-13 14:12:30 +0530

    syntax error in setup.py fixed

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 
 setup(
     name='pyar-de',
-    version='0,1',
+    version='0.1',
     packages=['pyar-de'],
     scripts=['pyar-de'],
     url='https://github.com/anooplab/pyar-de',
     license='GPL',
     author='anoop',
     author_email='anoop@chem.iitkgp.ac.in',
-    description='Global optimization of molecules using Differential Evolution in SciPy. Just a blackbox implementation.'
+    description='Global optimization of molecules using Differential Evolution '
+                'in SciPy. Just a blackbox implementation.'
 )
